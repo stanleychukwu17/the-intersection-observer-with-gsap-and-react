e6199a0adb3f8db02727fd0022cbd7072fe3eea8
stanley chukwu <stanleychukwu17@yahoo.com>
2022-09-28 19:14:15 +0100

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import './app.scss';
+import { motion } from 'framer-motion';
+import { gsap } from 'gsap';
+
+const App = () => {
+
+    return (
+        <div className="AppMain">
+
+        </div>
+    )
+}
+export default App;
