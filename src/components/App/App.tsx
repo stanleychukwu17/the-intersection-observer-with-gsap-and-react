6d744c70db751b0c5781db89f2e0a524ee291ca3
stanley chukwu <stanleychukwu17@yahoo.com>
2022-09-29 14:51:07 +0100

    finished the design of the top section

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -12,12 +12,22 @@ const App = () => {
     return (
         <div className="AppMain">
             <div className="TopBar">
-                <div className=""><img src={back} alt="" /></div>
-                <div className="">Intersection</div>
-                <div className=""><img src={msg} alt="" /></div>
+                <div className="TopBar__icon"><img src={back} alt="" /></div>
+                <div className="TopBar__hd">Intersection</div>
+                <div className="TopBar__icon"><img src={msg} alt="" /></div>
             </div>
             <div className="hero-section">
-                <img src={hero} alt="" />
+                <div className=""><img src={hero} alt="" /></div>
+            </div>
+            <div className="MidCvr">
+                <div className="Gname">Stanley Chukwu</div>
+                <div className="GText">
+                    Commodo velit aliqua quis in voluptate. Eu dolor dolor proident consectetur quis ea quis eiusmod exercitation ad dolor consequat. Exercitation ex deserunt magna Lorem. Minim pariatur do occaecat esse elit. Minim laboris do quis ullamco excepteur et mollit sit exercitation.
+                    In sint nisi duis duis ex. Excepteur nisi ipsum magna reprehenderit excepteur adipisicing non anim magna exercitation esse aliqua. Incididunt veniam esse ullamco cillum fugiat sunt ex. Nostrud eiusmod nulla irure proident duis culpa proident. Qui amet mollit eiusmod non consequat amet nisi nulla tempor culpa irure occaecat dolor.
+                    Ad ipsum officia dolor voluptate dolor officia irure labore ex. Sunt cillum qui elit officia eu. Laboris amet qui amet veniam. Est nostrud pariatur officia nulla ex. Esse cupidatat aliqua proident laboris veniam veniam ipsum.
+                    Ad ipsum officia dolor voluptate dolor officia irure labore ex. Sunt cillum qui elit officia eu. Laboris amet qui amet veniam. Est nostrud pariatur officia nulla ex. Esse cupidatat aliqua proident laboris veniam veniam ipsum.
+                    Ad ipsum officia dolor voluptate dolor officia irure labore ex. Sunt cillum qui elit officia eu. Laboris amet qui amet veniam. Est nostrud pariatur officia nulla ex. Esse cupidatat aliqua proident laboris veniam veniam ipsum.
+                </div>
             </div>
         </div>
     )
