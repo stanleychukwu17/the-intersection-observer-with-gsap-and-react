7ef8d1f2819b457d71d53c12f32ca12070da9e74
stanley chukwu <stanleychukwu17@yahoo.com>
2022-09-29 14:15:54 +0100

    imported all the assets into the App.tsx file and also kicked off the styling of this app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,14 +2,22 @@
 
 import './app.scss';
 
+// importing of assets
+import back from '../../assets/back-icon.svg'
+import msg from '../../assets/msg-icon.svg'
+import hero from '../../assets/hero.jpg'
+
 const App = () => {
 
     return (
         <div className="AppMain">
-            <div className="">
-                <div className="">back</div>
+            <div className="TopBar">
+                <div className=""><img src={back} alt="" /></div>
                 <div className="">Intersection</div>
-                <div className="">message</div>
+                <div className=""><img src={msg} alt="" /></div>
+            </div>
+            <div className="hero-section">
+                <img src={hero} alt="" />
             </div>
         </div>
     )
