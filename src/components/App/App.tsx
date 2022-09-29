4880f4af12c37f854a639dc508b240d467ab99f4
stanley chukwu <stanleychukwu17@yahoo.com>
2022-09-29 15:05:35 +0100

    changed the icons, worked on it so it could look better

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,8 +3,8 @@
 import './app.scss';
 
 // importing of assets
-import back from '../../assets/back-icon.svg'
-import msg from '../../assets/msg-icon.svg'
+import back from '../../assets/back3.svg'
+import msg from '../../assets/msg3.svg'
 import hero from '../../assets/hero.jpg'
 
 const App = () => {
@@ -20,7 +20,7 @@ const App = () => {
                 <div className=""><img src={hero} alt="" /></div>
             </div>
             <div className="MidCvr">
-                <div className="Gname">Stanley Chukwu</div>
+                <div className="GName">Stanley Chukwu</div>
                 <div className="GText">
                     Commodo velit aliqua quis in voluptate. Eu dolor dolor proident consectetur quis ea quis eiusmod exercitation ad dolor consequat. Exercitation ex deserunt magna Lorem. Minim pariatur do occaecat esse elit. Minim laboris do quis ullamco excepteur et mollit sit exercitation.
                     In sint nisi duis duis ex. Excepteur nisi ipsum magna reprehenderit excepteur adipisicing non anim magna exercitation esse aliqua. Incididunt veniam esse ullamco cillum fugiat sunt ex. Nostrud eiusmod nulla irure proident duis culpa proident. Qui amet mollit eiusmod non consequat amet nisi nulla tempor culpa irure occaecat dolor.
@@ -29,6 +29,16 @@ const App = () => {
                     Ad ipsum officia dolor voluptate dolor officia irure labore ex. Sunt cillum qui elit officia eu. Laboris amet qui amet veniam. Est nostrud pariatur officia nulla ex. Esse cupidatat aliqua proident laboris veniam veniam ipsum.
                 </div>
             </div>
+            <div className="LastCvr">
+                <div className="LastDts">
+                    Commodo velit aliqua quis in voluptate. Eu dolor dolor proident consectetur quis ea quis eiusmod exercitation ad dolor consequat. Exercitation ex deserunt magna Lorem. Minim pariatur do occaecat esse elit. Minim laboris do quis ullamco excepteur et mollit sit exercitation.
+                    In sint nisi duis duis ex. Excepteur nisi ipsum magna reprehenderit excepteur adipisicing non anim magna exercitation esse aliqua. Incididunt veniam esse ullamco cillum fugiat sunt ex. Nostrud eiusmod nulla irure proident duis culpa proident. Qui amet mollit eiusmod non consequat amet nisi nulla tempor culpa irure occaecat dolor.
+                    Ad ipsum officia dolor voluptate dolor officia irure labore ex. Sunt cillum qui elit officia eu. Laboris amet qui amet veniam. Est nostrud pariatur officia nulla ex. Esse cupidatat aliqua proident laboris veniam veniam ipsum.
+                    Ad ipsum officia dolor voluptate dolor officia irure labore ex. Sunt cillum qui elit officia eu. Laboris amet qui amet veniam. Est nostrud pariatur officia nulla ex. Esse cupidatat aliqua proident laboris veniam veniam ipsum.
+                    Ad ipsum officia dolor voluptate dolor officia irure labore ex. Sunt cillum qui elit officia eu. Laboris amet qui amet veniam. Est nostrud pariatur officia nulla ex. Esse cupidatat aliqua proident laboris veniam veniam ipsum.
+                </div>
+            </div>
+
         </div>
     )
 }
