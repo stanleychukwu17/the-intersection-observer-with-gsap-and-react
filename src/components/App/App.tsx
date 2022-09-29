0408d2e8413d26c41c4d58e1e92fe3ae0f8940b2
stanley chukwu <stanleychukwu17@yahoo.com>
2022-09-29 15:19:43 +0100

    finished the design of the 2nd part

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
         <div className="AppMain">
             <div className="TopBar">
                 <div className="TopBar__icon"><img src={back} alt="" /></div>
-                <div className="TopBar__hd">Intersection</div>
+                <div className="TopBar__hd">Summer Time</div>
                 <div className="TopBar__icon"><img src={msg} alt="" /></div>
             </div>
             <div className="hero-section">
@@ -30,12 +30,20 @@ const App = () => {
                 </div>
             </div>
             <div className="LastCvr">
-                <div className="LastDts">
+                <div className="GName">Summer time in Ukraine is fun with endless activities</div>
+                <div className="GText">
                     Commodo velit aliqua quis in voluptate. Eu dolor dolor proident consectetur quis ea quis eiusmod exercitation ad dolor consequat. Exercitation ex deserunt magna Lorem. Minim pariatur do occaecat esse elit. Minim laboris do quis ullamco excepteur et mollit sit exercitation.
                     In sint nisi duis duis ex. Excepteur nisi ipsum magna reprehenderit excepteur adipisicing non anim magna exercitation esse aliqua. Incididunt veniam esse ullamco cillum fugiat sunt ex. Nostrud eiusmod nulla irure proident duis culpa proident. Qui amet mollit eiusmod non consequat amet nisi nulla tempor culpa irure occaecat dolor.
                     Ad ipsum officia dolor voluptate dolor officia irure labore ex. Sunt cillum qui elit officia eu. Laboris amet qui amet veniam. Est nostrud pariatur officia nulla ex. Esse cupidatat aliqua proident laboris veniam veniam ipsum.
                     Ad ipsum officia dolor voluptate dolor officia irure labore ex. Sunt cillum qui elit officia eu. Laboris amet qui amet veniam. Est nostrud pariatur officia nulla ex. Esse cupidatat aliqua proident laboris veniam veniam ipsum.
                     Ad ipsum officia dolor voluptate dolor officia irure labore ex. Sunt cillum qui elit officia eu. Laboris amet qui amet veniam. Est nostrud pariatur officia nulla ex. Esse cupidatat aliqua proident laboris veniam veniam ipsum.
+                    Commodo velit aliqua quis in voluptate. Eu dolor dolor proident consectetur quis ea quis eiusmod exercitation ad dolor consequat. Exercitation ex deserunt magna Lorem. Minim pariatur do occaecat esse elit. Minim laboris do quis ullamco excepteur et mollit sit exercitation.
+                    In sint nisi duis duis ex. Excepteur nisi ipsum magna reprehenderit excepteur adipisicing non anim magna exercitation esse aliqua. Incididunt veniam esse ullamco cillum fugiat sunt ex. Nostrud eiusmod nulla irure proident duis culpa proident. Qui amet mollit eiusmod non consequat amet nisi nulla tempor culpa irure occaecat dolor.
+                    Ad ipsum officia dolor voluptate dolor officia irure labore ex. Sunt cillum qui elit officia eu. Laboris amet qui amet veniam. Est nostrud pariatur officia nulla ex. Esse cupidatat aliqua proident laboris veniam veniam ipsum.
+                    Ad ipsum officia dolor voluptate dolor officia irure labore ex. Sunt cillum qui elit officia eu. Laboris amet qui amet veniam. Est nostrud pariatur officia nulla ex. Esse cupidatat aliqua proident laboris veniam veniam ipsum.
+                </div>
+                <div className="">
+                    <button>$ Book your summer now</button>
                 </div>
             </div>
 
